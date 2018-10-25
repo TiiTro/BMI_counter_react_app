0adcbe41a6839ff995f14b52a1633bd22b07a9d5
Tiia <tiiatrogen@gmail.com>
2018-10-25 17:34:42 +0300

    Made some changes/added js component files

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,15 @@
+import styles from './card.scss';
+
+class Card extends React.Component {
+    render() {
+        console.log("This is the card component")
+        return(
+            <div className="card__container">
+                <div ClassName="card__body">
+                    
+                
+                </div>
+            </div>
+        )
+    }
+}
